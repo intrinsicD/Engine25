0eb11582058e2dfc6e01940b2b70e22fb46a8c27
intrinsicD <intrinsicd@posteo.de>
2025-01-07 14:28:49 +0100

    update

---
+++ Core/Frameworks/Geometry/AABB.h
@@ -0,0 +1,8 @@
+//
+// Created by alex on 1/7/25.
+//
+
+#ifndef AABB_H
+#define AABB_H
+
+#endif //AABB_H
---
+++ Core/Frameworks/Math.h
@@ -0,0 +1,8 @@
+//
+// Created by alex on 1/7/25.
+//
+
+#ifndef MATH_H
+#define MATH_H
+
+#endif //MATH_H
