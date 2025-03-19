104797f624a6a0e5f651fec671d20fc5ae0c2c7e
dieckman <dieckman@cs.uni-bonn.de>
2025-03-19 09:52:51 +0100

    added libtorch as dependency

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
 #include "Engine.h"
+#include <cuda_runtime.h>
+#include <iostream>
 
 int main() {
+
+    int deviceCount = 0;
+    cudaGetDeviceCount(&deviceCount);
+    for (int dev = 0; dev < deviceCount; ++dev) {
+        cudaDeviceProp deviceProp;
+        cudaGetDeviceProperties(&deviceProp, dev);
+        std::cout << "Device " << dev << ": " << deviceProp.name << "\n";
+        std::cout << "  Compute capability: "
+                  << deviceProp.major << "." << deviceProp.minor << "\n";
+    }
 
     Bcg::Engine engine;
     engine.init();
